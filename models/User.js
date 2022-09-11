3fa862a325cb066508434d6e5ec7cc3e6ccc743c
namanyash <naman_yash@yahoo.in>
2022-09-11 14:38:51 +1000

    Register | Login | GetUser Implemented

--- models/User.js
+++ models/User.js
@@ -1,17 +1,43 @@
 const mongoose =  require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: {
+    firstName: {
         type: String,
         required: true,
+    },    
+    lastName: {
+        type: String,
+        required: true,
+    },    
+    username: {
+        type: String,
+        required: true,
+        unique: true
     },
     email: {
         type: String,
         required: true,
         unique: true
     },
+    phoneNumber: {
+        type: Number,
+        required: true,
+        unique: true
+    },
     password: {
         type: String,
+        required: true,
+    },
+    balance: {
+        type: Number,
+        required: true,
+    },
+    activeRide: {
+        type: Object,
+        required: true,
+    },
+    rideHistory: {
+        type: Array,
         required: true,
     }
 })
--- routes/api/users.js
+++ routes/api/users.js
@@ -5,32 +5,56 @@ const {check, validationResult}  = require('express-validator');
 const User = require("../../models/User");
 const jwt = require('jsonwebtoken');
 const config =  require('config');
+const auth = require('../../middleware/auth');
 
 
-// @route       POST /api/users
+
+// @route       POST /api/users/register
 // @desc        Register new users with the app
 // @access      Public
-router.post('/',[
-    check('name', 'Name is required').not().isEmpty(),
+// @parameters  firstName(String), lastName(String), email(String), password(String), username(String), phoneNumber(Number)
+router.post('/register',[
+    check('firstName', 'Frist name is required').not().isEmpty(),
+    check('lastName', 'Frist name is required').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Please enter a password with 8 or more characters').isLength({min: 8})
+    check('password', 'Please enter a password with 8 or more characters').isLength({min: 8}),
+    check('username', 'Please enter a username with 4 or more characters').isLength({min: 4}),
+    check('phoneNumber', 'Please enter a phone number with 10 digits').isLength(10),
+    check('phoneNumber', 'Please enter a phone number with 10 digits').isLength({max: 10}),
+    check('phoneNumber', 'Phone Number can only be digits').isInt(),
 ] ,async (req,res)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()})
     }
-    const {name, email, password} = req.body;
+    const {firstName, lastName, email, password, username, phoneNumber} = req.body;
 
     try{
         let user = await User.findOne({email});
         if(user){
-            return res.status(400).json({errors:[{msg: 'User already exists'}]});
+            return res.status(400).json({errors:[{msg: 'Email already registered'}]});
+        }
+
+        user = await User.findOne({phoneNumber});
+        if(user){
+            return res.status(400).json({errors:[{msg: 'Phone Number already registered'}]});
+        }
+
+        user = await User.findOne({username});
+        if(user){
+            return res.status(400).json({errors:[{msg: 'Username already exists'}]});
         }
 
         user = new User({
-            name,
+            firstName,
+            lastName,
             email,
-            password
+            password,
+            username,
+            phoneNumber,
+            balance: 0,
+            activeRide: {},
+            rideHistory: []
         })
 
         const salt = await bcrypt.genSalt(10);
@@ -55,4 +79,56 @@ router.post('/',[
     }
 })
 
+// @route       POST /api/users/loginUP
+// @desc        Login users to the app using username and password
+// @access      Public
+// @parameters  username(String), password(String)
+router.post('/loginUP',[
+    check('password', 'Please enter a password with 8 or more characters').isLength({min: 8}),
+    check('username', 'Please enter a username with 4 or more characters').isLength({min: 4}),
+] ,async (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+    const {password, username} = req.body;
+
+    try{
+        let user = await User.findOne({username});
+        if(!user){
+            return res.status(400).json({errors:[{msg: 'Invalid Credentials'}]});
+        }
+        
+        isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({errors:[{msg: 'Invalid Credentials'}]});
+        }
+
+        const payload = {
+            user:{
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'),{expiresIn: 3600}, (err, token)=>{
+            if(err) throw err;
+            return res.json({token});
+        });
+    } catch(err) {
+        console.error(err.message);
+        return res.status(500).send('Server Error')
+    }
+})
+
+
+// @route       GET /api/users/getUser
+// @desc        Get information of a user
+// @access      Private
+// @parameters  
+router.get('/getUser',auth,async (req,res)=> {
+    const user = await User.findById(req.user.id).select('-password');
+    res.send(user)
+})
+
 module.exports = router;
+
